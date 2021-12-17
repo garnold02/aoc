0c37d2cb40d88f7618b5a4d412f0e80598760705
garnold02 <gulyas.arnold02@protonmail.com>
2021-12-17 18:34:12 +0100

    Setup

---
+++ common/stdsol/src/lib.rs
@@ -0,0 +1,41 @@
+use std::fmt::{Debug, Display, Formatter};
+use std::time::{Duration, Instant};
+
+pub fn solve<A, B>(year: i32, day: i32, part1: impl Fn() -> A, part2: impl Fn() -> B)
+where
+    A: Debug,
+    B: Debug,
+{
+    println!("Advent of Code {} - Day {}", year, day);
+
+    let start = Instant::now();
+    let part1 = part1();
+    let middle = Instant::now();
+    let part2 = part2();
+    let end = Instant::now();
+
+    println!("Part 1: {:?} ({})", part1, (middle - start).into());
+    println!("Part 2: {:?} ({})", part2, (end - middle).into());
+}
+
+struct Time {
+    duration: Duration,
+}
+
+impl From<Duration> for Time {
+    fn from(duration: Duration) -> Self {
+        Self { duration }
+    }
+}
+
+impl Display for Time {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        if self.duration.as_micros() < 1000 {
+            write!(f, "{}µs", self.duration.as_micros())
+        } else if self.duration.as_millis() < 1000 {
+            write!(f, "{}ms", self.duration.as_millis())
+        } else {
+            write!(f, "{}s", self.duration.as_secs())
+        }
+    }
+}
